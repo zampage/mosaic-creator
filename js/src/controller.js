364f7fb23f8eeee4e3f11e362562e71a31e85c19
Markus Chiarot <cmarkus@hotmail.ch>
2016-11-20 18:57:41 +0100

    load image and create canvas

--- js/src/controller.js
+++ js/src/controller.js
@@ -1,2 +1,13 @@
-console.log(_);
-console.log($);
+var $canvas, ctx, originalImage;
+
+getExampleImage()
+    .then(setupCanvas)
+    .then(function(data){
+        $canvas = data[0];
+        ctx = data[1];
+        originalImage = data[2];
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+
--- js/src/ui-handler.js
+++ js/src/ui-handler.js
@@ -1,17 +1,3 @@
-var $canvas, ctx, originalImage;
-
-getExampleImage()
-    .then(setupCanvas)
-    .then(function(data){
-        $canvas = data[0];
-        ctx = data[1];
-        originalImage = data[2];
-    })
-    .catch(function(error){
-        console.log(error);
-    });
-
-
 function getExampleImage(){
 
     return new Promise(function(resolve, reject){
